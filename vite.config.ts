380558410b3b4a6337c8fdbc3b046a83f737b857
Alan Fudge <alan@poetic.io>
2023-02-03 13:45:19 -0600

    load environment variable into vite config so that API endpoint can be configured based upon environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://4fne38anc0.execute-api.us-east-1.amazonaws.com/dev/poeticmaps',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_ENDPOINT,
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-});
+  });
+};

f83bf35f1d456b0af333fc2da083f3294f166962
Alan Fudge <alan@poetic.io>
2023-05-24 15:50:33 -0500

    set carousel to be optional

--- src/components/MapIndicatorModal/index.tsx
+++ src/components/MapIndicatorModal/index.tsx
@@ -15,22 +15,24 @@ const MapIndicatorModal = ({
 }: Props) => {
   return (
     <div className={[styles.mapIndicatorModal, selected ? styles.selected : null].join(' ')}>
-      <Carousel
-        className={styles.carouselWrapper}
-        showIndicators={false}
-        infiniteLoop={true}
-        showStatus={false}
-        showArrows={true}
-        showThumbs={false}
-      >
-        {modalImages.map(({ url, alt }, index) => {
-          return (
-            <div className={styles.carouselImageWrapper} key={`carousel-image-${index}`}>
-              <img src={url} alt={alt ? alt : undefined} />
-            </div>
-          );
-        })}
-      </Carousel>
+      {modalImages.length < 1 ? null : (
+        <Carousel
+          className={styles.carouselWrapper}
+          showIndicators={false}
+          infiniteLoop={true}
+          showStatus={false}
+          showArrows={true}
+          showThumbs={false}
+        >
+          {modalImages.map(({ url, alt }, index) => {
+            return (
+              <div className={styles.carouselImageWrapper} key={`carousel-image-${index}`}>
+                <img src={url} alt={alt ? alt : undefined} />
+              </div>
+            );
+          })}
+        </Carousel>
+      )}
       <div className={styles.textWrapper}>
         <h3 className={styles.title}>{name}</h3>
         <span className={styles.address}>{address}</span>
